1389e1274eec73b6dcf8eb6daf94e7ea5c6f87fb
Sam Nelson <samn10.nelson@gmail.com>
2025-02-27 20:54:34 -0800

    completed assignment push2

--- cypress/e2e/formspec.cy.js
+++ cypress/e2e/formspec.cy.js
@@ -17,7 +17,6 @@ describe('Form-Testing', () => {
        cy.contains('li', 'seafood').should('be.visible');
        cy.contains('li', '5').should('be.visible');
      });
- 
   })
   
   it("Creates a booking2", () => {
@@ -35,6 +34,39 @@ describe('Form-Testing', () => {
        cy.contains('li', 'Everything').should('be.visible');
        cy.contains('li', '5').should('be.visible');
      });
+  })
+  
+  it("Creates two bookings", () => {
+    cy.get('[data-cy="input-name"]').type("sam");
+    cy.get('[data-cy="input-address"]').type("home");
+    cy.get('[data-cy="input-phone"]').type("206");
+    cy.get('[data-cy="input-cuisine"]').type("seafood");
+    cy.get('[data-cy="input-rating"]').type("5");
+    
+    cy.get('[data-cy="rating-form"]').submit();
+    
+    cy.get(`[data-cy="sam"] ul`).within(() => {
+       cy.contains('li', 'home').should('be.visible');
+       cy.contains('li', '206').should('be.visible');
+       cy.contains('li', 'seafood').should('be.visible');
+       cy.contains('li', '5').should('be.visible');
+     });
+     
+    cy.get('[data-cy="input-name"]').type("TestResturant");
+    cy.get('[data-cy="input-address"]').type("_____");
+    cy.get('[data-cy="input-phone"]').type("206-111-1111");
+    cy.get('[data-cy="input-cuisine"]').type("Everything");
+    cy.get('[data-cy="input-rating"]').type("5");
+    
+    cy.get('[data-cy="rating-form"]').submit();
+    
+    cy.get(`[data-cy="TestResturant"] ul`).within(() => {
+       cy.contains('li', '_____').should('be.visible');
+       cy.contains('li', '206-111-1111').should('be.visible');
+       cy.contains('li', 'Everything').should('be.visible');
+       cy.contains('li', '5').should('be.visible');
+     });
  
   })
+  
 })
